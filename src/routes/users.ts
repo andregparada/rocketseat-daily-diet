bc22b60f11ec7909abc74f88e149f59d0f26cd02
André <andregparada@gmail.com>
2024-05-06 14:29:20 -0300

    feat: middlewares check-session-id-exists and get users routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -34,4 +34,28 @@ export async function usersRoutes(app: FastifyInstance) {
 
     return reply.status(201).send()
   })
+
+  app.get('/', async (request) => {
+    const { sessionId } = request.cookies
+
+    const users = await knex('users').where('session_id', sessionId).select()
+
+    return { users }
+  })
+
+  app.get('/:id', async (request) => {
+    const getUserParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getUserParamsSchema.parse(request.params)
+
+    const { sessionId } = request.cookies
+
+    const user = await knex('users')
+      .where({ session_id: sessionId, id })
+      .first()
+
+    return { user }
+  })
 }
